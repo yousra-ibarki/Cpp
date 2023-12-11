b966263ad57ee07518e4182975649247821923e3
Yousra Ibarki <yoibarki@e3r7p12.1337.ma>
2023-12-11 10:08:44 +0100

    .

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -1,4 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoibarki <yoibarki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/09 10:57:48 by yoibarki          #+#    #+#             */
+/*   Updated: 2023/12/09 15:28:11 by yoibarki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <cstdlib>
 #include <string>
 
 class Contact
@@ -11,7 +24,6 @@ private:
     std::string dark_secret;
 
 public:
-    Contact() : first_name(10, '\0'), last_name(10, '\0'), nickname(10, '\0'){}
 
     void ft_add_name(const std::string &new_name, const std::string &new_last, const std::string &new_nickname)
     {
@@ -31,9 +43,10 @@ public:
                   << nickname << "|"
                   << "\n";
     }
-    void ft_print2() const{
-        std::cout << "First Name:         " <<first_name << std::endl;
-        std::cout << "Last Name:          "  << last_name << std::endl;
+    void ft_print2() const
+    {
+        std::cout << "First Name:         " << first_name << std::endl;
+        std::cout << "Last Name:          " << last_name << std::endl;
         std::cout << "Nickname:           " << nickname << std::endl;
         std::cout << "Phone Number:       " << nbr_phone << std::endl;
         std::cout << "The Darkest Secret: " << dark_secret << std::endl;
@@ -67,7 +80,6 @@ public:
             contacts[i].ft_add_nbrPhone(nbrPhone, secret);
             i = (i + 1) % max_size;
             count_contacts++;
-            
         }
     }
 
@@ -85,7 +97,8 @@ public:
         }
     }
 
-    void ft_searchable(int index) const{
+    void ft_searchable(int index) const
+    {
         contacts[index].ft_print2();
     }
 };
@@ -103,38 +116,38 @@ std::string ft_commands()
 {
     std::string command;
 
-    // do
-    // {
-        std::cout << "\nEnter One Of The Following Commands :\nADD:    Save a new contact\nSEARCH: Display a specific contact\nEXIT:   Quit\n";
-        std::getline(std::cin, command);
-    // } while (command != "ADD" && command != "SEARCH" && command != "EXIT");
+    std::cout << "\nEnter One Of The Following Commands :\nADD:    Save a new contact\nSEARCH: Display a specific contact\nEXIT:   Quit\n";
+    std::getline(std::cin, command);
     return command;
 }
 
-
-
 std::string ft_space(std::string str, std::string out)
 {
-    int i = 0;
+    int i;
     int j = 0;
     int len = str.length();
-   
-    while(i < (10 - len))
-    {
-        out[i] = ' ';
-        i++;
-    }
-   while(i < 10)
+
+    i = (10 - len);
+    while (i < 10)
     {
         out[i] = str[j];
         i++;
         j++;
-        if(len > 10)
+        if (len > 10)
             out[9] = '.';
     }
     return out;
-} 
+}
 
+bool checkDigit(std::string str)
+{    
+    for(int i = 0; str[i] ; i++)
+    {
+        if(!std::isdigit(str[i]))
+            return false;
+    }
+    return true;
+}
 int main()
 {
     PhoneBook book;
@@ -142,7 +155,8 @@ int main()
     std::string command;
     std::string out(10, ' ');
     int count_contacts;
-    int index;
+    std::string index;
+    int int_index;
     int i;
 
     i = 0;
@@ -152,37 +166,52 @@ int main()
         command = ft_commands();
         if (command == "ADD")
         {
-            std::cout << "Enter The First Name ";
+            std::cout << "Enter The First Name :";
             std::getline(std::cin, contact.first_name);
-            contact.first_name = ft_space(contact.first_name, out);
+           contact.first_name = ft_space(contact.first_name, out);
 
-            std::cout << "Enter The Last Name ";
+            std::cout << "Enter The Last Name :";
             std::getline(std::cin, contact.last_name);
             contact.last_name = ft_space(contact.last_name, out);
 
-            std::cout << "Enter The Nickname ";
+            std::cout << "Enter The Nickname :";
             std::getline(std::cin, contact.nickname);
             contact.nickname = ft_space(contact.nickname, out);
 
-            std::cout << "Enter The Phone Number ";
+            std::cout << "Enter The Phone Number :";
             std::getline(std::cin, contact.nbr_phone);
 
-            std::cout << "Enter The Darkest Secret ";
+            std::cout << "Enter The Darkest Secret :";
             std::getline(std::cin, contact.dark_secret);
-            
+
             book.ft_add_to_PhoneBook(contact.first_name, contact.last_name, contact.nickname, contact.nbr_phone, contact.dark_secret);
             std::cout << "\nYour Contact Has Been Added Seccessfully.\n";
             count_contacts++;
         }
         else if (command == "SEARCH")
-        {
+        { 
             book.ft_display_PhoneBook();
             std::cout << "\nEnter The Index Of The Contact That You Want To See\n";
-            std::cin >> index;
-            if(index < 0 || index > count_contacts)
-                std::cout << "Index Out Of Range";
+            std::getline(std::cin, index);
+            int_index = std::atoi(index.c_str());
+            if (int_index < 0 || int_index > count_contacts || !checkDigit(index))
+                std::cout << "\nIncorrect Or Index Out Of Range\n";
             else
-                book.ft_searchable(index-1);
+                book.ft_searchable(int_index - 1);
         }
     } while (command != "EXIT");
 }
+
+// ctr-d
+// cin.good
+// cin.empty
+// getline()
+// using namespace std;
+// int main()
+// {
+//     string str;
+//     getline(cin, str);
+//     if (!cin.good())exit(0);
+//     while(str.empty())
+//         getline(cin, str);
+// }
---
+++ CPP01/ex00/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+class Zombie{
+private:
+std::string name;
+
+
+public:
+
+void announce()
+{
+    std::cout << "BraiiiiiiinnnzzzZ...";
+}
+
+};
+
+Zombie* newZombie( std::string name )
+{
+    Zombie zombie;
+
+    zombie.
+}
+
+int main()
+{
+    Zombie zombie;
+
+}
