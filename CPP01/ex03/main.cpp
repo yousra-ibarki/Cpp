71f2f5891cbd44b4ae81f11660085640c2d40a9a
Yousra Ibarki <yoibarki@e3r7p11.1337.ma>
2023-12-16 12:39:08 +0100

    .

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -17,33 +17,45 @@ public:
 
 class HumanA{
 private:
-    Weapon weapon;
+    Weapon *weapon;
     std::string name;
 public:
     HumanA(){
-        
+        weapon = new Weapon;
+    }
+    ~HumanA(){
+        delete weapon;
+    }
+    void set_value(const std::string new_name){
+        name = new_name;
     }
     void attack(){
-        std::cout << name << " attacks with their " << weapon.getType();
+        std::cout << name << " attacks with their " << weapon->getType();
     }
 
 };
 
-class HumanB{
-private:
-    Weapon weapon;
-    std::string name;
-public:
-    void attack(){
-        std::cout << name << " attacks with their " << weapon.getType();
-    }
+// class HumanB{
+// private:
+//     Weapon weapon;
+//     std::string name;
+// public:
+//     void attack(){
+//         std::cout << name << " attacks with their " << weapon.getType();
+//     }
 
-};
+// };
 
 int main()
 {
     Weapon weapon;
-    weapon.setType("Hello");
-    const std::string &ref = weapon.getType();
-    std::cout << ref;
+    HumanA human1;
+
+    human1.set_value("human1");
+    weapon.setType("GOMO GOMO");
+    //const std::string &ref = weapon.getType();
+    //std::cout << ref;
+   // std::cout << "Weapon Type: " << weapon.getType() << std::endl;
+    human1.set_value("Yousra");
+    human1.attack();
 }
