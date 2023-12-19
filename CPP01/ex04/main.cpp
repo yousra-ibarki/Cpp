d3e7f8e5a74534ed1eb1569e6279d4944fb4c765
Smile <yousraibarki@Agashi.local>
2023-12-19 19:00:49 +0100

    .

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -5,16 +5,24 @@
 #include <fstream>
 
 
-std::string ft_replace(int pos, std::string line, std::string str){
-    // std::cout << pos << std::endl;
-    // std::cout << "this is the output" << "\t" << line[pos] << std::endl;
-   // for(int j = 0; j < strlen(line.c_str()); j++){
-        for(int i = 0; i < strlen(str.c_str()); i++){
-            line[pos] = str[i];
-            i++;
-            pos++;
-        }
-    //}
+int ft_count(std::string line, std::string str1){
+    int count = 0;
+    int i = 0;
+    int pos = 0;
+    while(i < strlen(line.c_str())){
+      pos += line.find(str1);
+      count++;
+      i++;
+    }
+   std::cout << count << std::endl;
+    return count;
+}
+
+std::string ft_replace(int pos, std::string line, std::string str2, std::string str1){
+    int count = ft_count(line, str1);
+    line.insert(pos, str2);
+    pos = line.find(str1);
+    line.erase(pos, strlen(str1.c_str()));
     return line;
 }
 
@@ -30,13 +38,16 @@ int main(int ac, char **av)
         std::string line;
         const char *linee;
         int repeat_str = 0;
+        int pos;
 
         std::ifstream inputFile(file_name);
         std::ofstream outputFile(file_name+".replace");
         
         if(inputFile.is_open() && outputFile.is_open()){
            while(std::getline(inputFile, line)){
-                line = ft_replace(line.find(s1), line, s2);
+                pos = line.find(s1);
+               if(pos != std::string::npos)
+                    line = ft_replace(pos, line, s2, s1);
                 outputFile << line << std::endl;
            }
             inputFile.close();
