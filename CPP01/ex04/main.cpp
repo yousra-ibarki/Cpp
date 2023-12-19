9e038f97ff39927273c93e1143c032afa09e583f
Smile <yousraibarki@Agashi.local>
2023-12-19 12:10:08 +0100

    .

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -2,65 +2,34 @@
 #include <string>
 #include <stdio.h>
 #include <fcntl.h>
+#include <fstream>
 
 int main(int ac, char **av)
 {
     if (ac <= 3)
-        std::cout << "Error" << std::endl;
+        std::cout << "Usage: ./program <filename> <s1> <s2>" << std::endl;
     else
     {
-
         std::string file_name = av[1];
         std::string s1 = av[2];
         std::string s2 = av[3];
- 
+        std::string line;
+        int repeat_str = 0;
 
         // std::cout << file_name << "\t" <<s1 << "\t" << s2 << std::endl;
-        open(av[1], O_RDWR | O_CREAT);
+       // open(av[1], O_RDWR);
+        std::ifstream inputFile(file_name);
+        std::ofstream outputFile(file_name+".replace");
         
+        if(inputFile.is_open() && outputFile.is_open()){
+           while(std::getline(inputFile, line)){
+                outputFile << line << std::endl;
+                repeat_str += line.find(s1);
+                //std::cout << "["<< line << "]" << std::endl;
+           }
+            inputFile.close();
+            outputFile.close();
+            std::cout << repeat_str;
+        }
     }
-}
-
-
-
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-void replaceContent(const std::string& filename, const std::string& s1, const std::string& s2) {
-    std::ifstream inputFile(filename);
-    if (!inputFile) {
-        std::cout << "File " << filename << " not found." << std::endl;
-        return;
-    }
-
-    std::stringstream buffer;
-    buffer << inputFile.rdbuf();
-    std::string content = buffer.str();
-
-    size_t pos = 0;
-    while ((pos = content.find(s1, pos)) != std::string::npos) {
-        content.replace(pos, s1.length(), s2);
-        pos += s2.length();
-    }
-
-    std::ofstream outputFile(filename + ".replace");
-    outputFile << content;
-
-    std::cout << "Content of " << filename << " has been copied to " << filename + ".replace" << " with replacements." << std::endl;
-}
-
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        std::cout << "Usage: ./program <filename> <s1> <s2>" << std::endl;
-        return 1;
-    }
-
-    std::string filename = argv[1];
-    std::string s1 = argv[2];
-    std::string s2 = argv[3];
-
-    replaceContent(filename, s1, s2);
-
-    return 0;
 }
