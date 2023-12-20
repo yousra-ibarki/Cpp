657a84f7920f99b1fad9dcee36491bb4af67dd74
Yousra Ibarki <yoibarki@e3r2p9.1337.ma>
2023-12-20 10:33:31 +0100

    .

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -9,10 +9,9 @@ int ft_count(std::string line, std::string str1){
     int count = 0;
     int i = 0;
     int pos = 0;
-    while(i < strlen(line.c_str())){
-      pos += line.find(str1);
+    while(((pos = line.find(str1, pos)) != std::string::npos)){
       count++;
-      i++;
+      pos += str1.size();
     }
    std::cout << count << std::endl;
     return count;
@@ -20,9 +19,11 @@ int ft_count(std::string line, std::string str1){
 
 std::string ft_replace(int pos, std::string line, std::string str2, std::string str1){
     int count = ft_count(line, str1);
+    for(int i = 0; i < count ; i++){
     line.insert(pos, str2);
     pos = line.find(str1);
     line.erase(pos, strlen(str1.c_str()));
+    }
     return line;
 }
 
