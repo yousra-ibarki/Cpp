88e8607e33afb01e23075872f2b17c06c8aaa3e7
Smile <yousraibarki@Agashi.local>
2023-12-20 12:57:15 +0100

    .

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -5,25 +5,35 @@
 class Harl{
 private:
     void debug(void){
-
+        std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
     }
     void info(void){
-
+        std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
     }
     void warning(void){
-
+        std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years where as you started working here since last month." << std::endl;
     }
     void error(void){
-
+        std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
     }
 public:
+    fc
     void complain(std::string level){
-
+        debug();
+        info();
+        warning();
+        error();
     }
 
 };
 
-int main()
+int main(int ac, char **av)
 {
-    
+    if(ac == 2)
+    {
+        Harl obj;
+        obj.complain(av[1]);   
+    }
+    else
+        std::cout << "Usage: <level: debug/info/warning/error>" << std::endl;
 }
