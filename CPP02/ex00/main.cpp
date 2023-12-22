660fb3875f1f9ab4a802875b33a814abe669cc45
Yousra Ibarki <yoibarki@e3r4p7.1337.ma>
2023-12-22 15:12:31 +0100

    .

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -5,14 +5,29 @@ private:
     int fixed_point;
     static const int fract_bit = 8;
 public:
-    //a default constructor
-    OCF(): fixed_point(0) {}
-    //a copy constructor
+    OCF(): fixed_point(0) {
+        std::cout << "Default Constructor\n";
+    }
     OCF(OCF &new_obj){
+        std::cout << "Copy Constructor\n";
         fixed_point = new_obj.fixed_point;
     }
-    //a copy assignment operator overload
-    
+    //a copy assignment operator overload is just a correction of 
+    //the default behavior of the shallow copying which lead to 
+    //the double free and leaks 
+    OCF& operator= (const OCF &var){
+        std::cout << "Copy Assignment Operator\n";
+        //this->fixed_point = var.fixed_point;  //copying fixed point of a in fixed point of b
+        fixed_point = var.fixed_point;
+    }
+
+
+
+
+    ~OCF(){
+        std::cout << "Destructor\n";
+        // delete var;
+    }
 
 
 
@@ -21,5 +36,9 @@ public:
 
 int main()
 {
+    OCF a;
+    OCF b;
+    OCF c;
 
+    a = b;  // means a.operator= (b);
 }
---
+++ CPP02/ex00/try.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Length 
+{
+    private:
+        int kmeter;            
+        int meter;           
+
+    public:
+
+        Length() //default constructor 
+        {
+            kmeter = 0;
+            meter = 0;
+        }
+        Length(int km, int m)  //overloaded constructor.
+        {
+            kmeter= km;
+            meter = m;
+        }
+        void operator = (const Length &l) 
+        { 
+            kmeter = l.kmeter;
+            meter = l.meter;
+        }
+
+        //method to display length.
+        void displayLength() 
+        {
+            cout << kmeter << " Kms "<< meter << " meters" << endl;
+        }
+};
+
+int main() 
+{
+    // Assigning by overloading contructor
+    Length len1(1, 100);
+    Length len2(2, 200);
+
+    cout <<"Len1 length: "; 
+    len1.displayLength();
+   
+    cout <<"Len2 length: "; 
+    len2.displayLength();
+
+    // overloading assignment operator to copy values
+    len1 = len2;
+
+    cout << "\nLen1 Length: "; 
+    len1.displayLength();
+    
+    cout << "Len2 Length: "; 
+    len2.displayLength();
+
+    return 0;
+}
