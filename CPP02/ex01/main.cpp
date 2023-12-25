58aed9c9b70caf00a5715c2b6a7be8c7c66f70f3
Yousra Ibarki <yoibarki@e3r7p18.1337.ma>
2023-12-25 17:20:51 +0100

    .

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -1,42 +1,52 @@
 #include <iostream>
+#include <cmath>
 
 class Fixed{
 private:
     int fixed_point;
     static const int fract_bit = 8;
 public:
-    Fixed (const int var) {
-        fixed_point = var;
-    }
-    Fixed (const float var1){
-
-    }
+//Default Constructor
     Fixed(): fixed_point(0) {
         std::cout << "Default Constructor called\n";
     }
-    Fixed(Fixed &new_obj){
+// Copy Constructor
+    Fixed(Fixed &new_obj): fixed_point(new_obj.fixed_point) {
         std::cout << "Copy Constructor called\n";
         fixed_point = new_obj.fixed_point;
     }
-
-    float toFloat( void) const{
-        //converting fixed point value to floating point value;
+//Int Constructor
+    Fixed (const int var) {
+        std::cout << "Int Constructor Called\n";
+        fixed_point = var *  pow(2, fract_bit);
+       // std::cout << fixed_point << std::endl;
+    }
+// Float Constructor
+    Fixed (const float var1){
+        std::cout << "Float Constructor Called\n";
+        int integer;
+        float fractional;
+        float fixed_point1 ;
+        fixed_point1 = var1 * pow(2, fract_bit);
+        integer = (int)fixed_point1;
+        fractional = (fixed_point1 - integer);        
+        if(fractional >= 0.5)
+            fixed_point = integer + 1;
+        else
+            fixed_point = integer;
+        //std::cout << "result : " << fixed_point << std::endl;
     }
 
-    int toInt( void )const{
-        //converting the fixed point to int 
-    }
-
-
+    Fixed& operator= (const Fixed &var){
     //a copy assignment operator overload is just a correction of 
     //the default behavior of the shallow copying which lead to 
     //the double free and leaks 
-    Fixed& operator= (const Fixed &var){
         std::cout << "Copy Assignment Operator called\n";
         //this->fixed_point = var.fixed_point;  //copying fixed point of a in fixed point of b
         fixed_point = var.fixed_point;
         return *this;
     }
+
     ~Fixed(){
         std::cout << "Destructor called\n";
         // delete var;
@@ -46,13 +56,23 @@ public:
         return fixed_point;
     }
     void setRawBits(int const raw){
-        // std::cout << "getRawBits memeber function called\n";
+         std::cout << "getRawBits memeber function called\n";
         fixed_point = raw;
     }
 
+    float toFloat( void) const{
+       return fixed_point / pow(2, fract_bit);
+    }
 
-
+    int toInt( void )const{
+       return fixed_point / pow(2, fract_bit);
+    }
 };
+
+std::ostream& operator<<(std::ostream& c, const Fixed& object){
+    c << "" << object.toFloat() << "";
+    return c;
+}
 
 
 int main( void )
@@ -61,14 +81,17 @@ int main( void )
     Fixed const b(10); 
     Fixed const c(42.42f); 
     Fixed const d(b); 
+
     a = Fixed(1234.4321f); 
-    std::cout << "ais" << a<<std::endl; 
-    std::cout << "bis" << b<<std::endl; 
-    std::cout <<"cis" << c<<std::endl; 
-    std::cout <<"dis" << d<<std::endl; 
-    std::cout <<"ais" << a.toInt()<< "asinteger"<<std::endl; 
-    std::cout <<"bis" << b.toInt()<< "asinteger"<<std::endl; 
-    std::cout <<"cis" << c.toInt()<< "asinteger"<<std::endl; 
-    std::cout <<"dis" << d.toInt()<< "asinteger"<<std::endl; 
+
+    std::cout << "a is " << a <<std::endl; 
+    std::cout << "b is " << b <<std::endl; 
+    std::cout << "c is " << c <<std::endl; 
+    std::cout << "d is " << d <<std::endl; 
+
+    std::cout << "a is " << a.toInt()<< " as integer"<<std::endl; 
+    std::cout << "b is " << b.toInt()<< " as integer"<<std::endl; 
+    std::cout << "c is " << c.toInt()<< " as integer"<<std::endl; 
+    std::cout << "d is " << d.toInt()<< " as integer"<<std::endl; 
     return 0; 
 }
