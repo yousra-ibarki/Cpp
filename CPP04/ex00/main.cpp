109224a64aaf2d1f1b99a4e36929a4739ad29315
Smile <yousraibarki@Agashi.local>
2024-01-23 01:25:39 +0100

    .

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -4,31 +4,41 @@
 #include "Cat.hpp"
 #include "WrongCat.hpp"
 
-
+void function(){
+    
+}
 
 int main() 
 { 
-    const WrongAnimal* metaa = new WrongAnimal(); 
-    const WrongAnimal* ii = new WrongCat(); 
-    std::cout << ii->getType() << ""<<std::endl; 
-    ii->makeSound();
-    metaa->makeSound(); 
-    
-    delete ii;
-    delete metaa;
-
-    std::cout << std::endl;
+    atexit(function);
+    std::cout << std::endl << "°°°°°°°°°°Right Animals°°°°°°°°°°" << std::endl << std::endl;
+    std::cout << "--Constructors--" << std::endl;
     const Animal* meta = new Animal(); 
     const Animal* j = new Dog(); 
     const Animal* i = new Cat(); 
+    std::cout << std::endl << "--Animals and their Sounds--" << std::endl;
+
     std::cout << j->getType() << ""<<std::endl; 
     std::cout << i->getType() << ""<<std::endl; 
     i->makeSound(); 
     j->makeSound(); 
-    meta->makeSound(); 
-    
+    meta->makeSound();
+    std::cout << std::endl << "--Destructors--" << std::endl;
     delete j;
     delete i;
     delete meta;
+
+    std::cout << std::endl << "°°°°°°Wrong Animals°°°°°°°°°°" << std::endl << std::endl;
+    std::cout << "--Constructors--" << std::endl;
+    const WrongAnimal* metaa = new WrongAnimal(); 
+    const WrongAnimal* ii = new WrongCat(); 
+    std::cout << std::endl << "--Wrong Animals and their Sounds--" << std::endl;
+    std::cout << ii->getType() << ""<<std::endl; 
+    ii->makeSound();
+    metaa->makeSound(); 
+    std::cout << std::endl << "--Destructors--" << std::endl;
+
+    delete ii;
+    delete metaa;
     return 0; 
 }
