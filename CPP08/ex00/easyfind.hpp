33b6990f6aaa5bdabb88cd35bfe267a9da7f2799
Yousra Ibarki <yoibarki@e3r3p2.1337.ma>
2024-02-21 17:19:28 +0100

    .

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -3,11 +3,19 @@
 
 #include <iostream>
 #include <vector>
+#include "algorithm"
 
 template <typename T>
 
-void easyfind(T arg, int nbr){
-    for(int i = 0; )
+void easyfind(const T &vec, unsigned long nbr){
+    std::cout << vec.size() << std::endl;
+
+    std::vector<const int* >::iterator out = std::find(vec.begin(), vec.end() , nbr);
+
+    if(out != vec.end())
+        std::cout << "the element " << nbr << " found at index " << std::endl;
+    else
+        throw(nbr);
 }
 
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -2,11 +2,21 @@
 
 int main(){
     std::vector<int> vec;
-    int size  = 15;
-    for(int i = 0; i < size; i++){
-        vec.push_back(i);
+    vec.push_back(5);
+    vec.push_back(10);
+    vec.push_back(95);
+    vec.push_back(57);
+    vec.push_back(23);
+    // for(int i = 0; i < size; i++){
+    //     vec.push_back(i);
+    // }
+    // for(int j = 0; j < size;  j++)
+    //     std::cout << j << std::endl;
+    try{
+        easyfind(vec, 10);
+
     }
-    for(int j = 0; j < size;  j++)
-        std::cout << j << std::endl;
-    easyfind(vec, 5)
+    catch(unsigned long a){
+        std::cout <<  a << " this occurrence did not found" << std::endl;
+    }
 }   
