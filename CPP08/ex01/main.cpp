4c7963d6f050340e3bce31c55e1867356158314b
Yousra Ibarki <yoibarki@e2r2p11.1337.ma>
2024-03-08 00:55:31 +0100

    .

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -2,17 +2,16 @@
 
 int main()
 {
-    Span sp = Span(100);
+    Span sp = Span(125);
     sp.addNumbers();
     // sp.addNumber(6);
     // sp.addNumber(3);
     // sp.addNumber(17);
     // sp.addNumber(9);
     // sp.addNumber(11);
-    // sp.addNumber(11);
-    sp.ft_display();
-    sp.longestSpan();
+    // sp.ft_display();
     sp.shortestSpan();
+    sp.longestSpan();
     // std::cout << sp.shortestSpan() << std::endl;
     // std::cout << sp.longestSpan() << std::endl;
     return 0;
